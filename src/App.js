102c772ec587ee5b325c67ed2ff648e44f682198
Christina <christina-jung@hotmail.com>
2020-09-08 10:13:33 +0200

    Navigation Drawer, first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import Toolbar from './components/Toolbar/Toolbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Toolbar/>
+     <main style={{marginTop: '64px'}}>
+     <p>This is page content</p>
+     </main>
     </div>
   );
 }
 
 export default App;
+
+// toolbar is fixed and does not work with paragraphs
+// paras do not respect toolbar
+// 
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,32 @@
+// allows us to write jsx code:
+import React from 'react';
+// this will handle css code:
+import './Toolbar.css';
+// >
+// li = link, ul = unorderedlist
+// returns Statement Header:
+// functional component, where I get props and return some jsx (currly braces)
+// return some value 
+const toolbar = props => (
+
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
+            <div></div>
+            <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+            <div className="spacer"/>
+            <div className="toolbar_navigation-items">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Meine WG</a></li>
+                    <li><a href="/">ToDo</a></li>
+                    <li><a href="/">Einkaufen</a></li>
+                    <li><a href="/">Putzen</a></li>
+                    <li><a href="/">Settings</a></li>
+                </ul>
+            </div>
+         
+        </nav>
+    </header>
+);
+
+export default toolbar;
